653da3eab979423b3dfab11e953cab0f5eff8175
USER <Damixl213>
2025-02-12 15:57:08 +0100

    All done

--- src/Components/XandO/X_O.jsx
+++ src/Components/XandO/X_O.jsx
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
-import React from 'react'
-import './X_O.css'
-import heart from '../Assets/heart.png'
-import broken_heart from '../Assets/brokenheart.png'
-export const X_O = () => {
-  return (
-    <div className='container'>
-      <h1 className="title">X and O love gaming <span>React</span></h1>
-
-      <div className="board">
-
-      </div>
-      <button className="reset">Restart</button>
-=======
+ 
 import React, { useRef, useState } from "react";
 import Confetti from 'react-confetti';
 import './X_O.css'
@@ -235,12 +221,9 @@ const reset = () => {
       </div>
       </div>
       <button className="reset" onClick={reset}>Restart</button>
->>>>>>> Tictactoe
+
     </div>
   )
 }
 export default X_O
-<<<<<<< HEAD
-xx
-=======
->>>>>>> Tictactoe
+
